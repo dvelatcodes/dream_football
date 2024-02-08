cf19417c28f3a819c941b4134f048df71de4bfc9
love olubummo <olubummolove3@gmail.com>
2024-02-08 10:06:58 +0100

    made ui presentable

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -3,56 +3,80 @@ import Quarterback from "./Quarterback.jpg";
 import timeout from "./timeout.jpg";
 import Behind from "./Behind.jpg";
 import Rivalry from "./Rivalry.jpg";
-import Innovations from "./Innovations.jpg";
+import Innovations from "./Innovation.jpg";
 import Mary from "./Mary.jpg";
 import Hero from "./Hero.jpg";
 import Kickoff from "./Kickoff.jpg";
-import Legacy from ".Legacy.jpg";
+import Legacy from "./Legacy.jpg";
+import { StaticImageData } from "next/image";
 
-export const story: {}[] = [
+export const story: Array<{
+  img: StaticImageData;
+  title: string;
+  desc: string;
+  blurDataURL: string;
+}> = [
   {
     img: Kickoff,
     title: "The Kickoff",
     desc: "It was a crisp fall afternoon, and the stadium buzzed with excitement as the teams geared up for the kickoff. Fans roared, and the anticipation hung in the air like static electricity",
+    blurDataURL:
+      "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FKickoff.b64414d7.jpg&w=8&q=70",
   },
   {
     img: Quarterback,
     title: "The Star Quarterback",
     desc: "Meet Jake, the charismatic quarterback with a rocket arm. Known for his precision passes, Jake led his team with unwavering determination, becoming the heartthrob of the football-crazed town",
+    blurDataURL:
+      "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FQuarterback.b31958da.jpg&w=5&q=70",
   },
   {
     img: timeout,
     title: "A Strategic Timeout",
     desc: "With seconds left on the clock, the coach called a timeout. Tension filled the huddle as the team strategized their next move, devising a play that could make or break their championship dreams.",
+    blurDataURL:
+      "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftimeout.95805bcc.jpg&w=5&q=70",
   },
   {
     img: Hero,
     title: "The Unlikely Hero",
     desc: "In a twist of fate, a rookie wide receiver, underestimated by opponents, caught the winning touchdown pass. The stadium erupted, celebrating the underdog's triumph and proving that anyone can be a hero on the gridiron.",
+    blurDataURL:
+      "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FHero.268cb113.jpg&w=8&q=70",
   },
   {
     img: Rivalry,
     title: "Epic Rivalry",
     desc: "The team's arch-nemesis challenged them in a rivalry as old as time. The clash between the titans electrified the atmosphere, transcending the game into a battle of wills",
+    blurDataURL:
+      "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FRivalry.706a82a4.jpg&w=5&q=70",
   },
   {
     img: Mary,
     title: "The Hail Mary",
     desc: "In a do-or-die situation, the team executed a Hail Mary pass. The ball soared through the air, weaving between defenders, culminating in a miraculous catch that etched itself into football folklore.",
+    blurDataURL:
+      "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FMary.753f5a91.jpg&w=5&q=70",
   },
   {
     img: Behind,
     title: "Behind the Scenes",
     desc: "Off the field, the players' camaraderie unfolded. From locker room banter to shared triumphs and defeats, their bond extended beyond the game, mirroring the spirit of teamwork celebrated in football short stories",
+    blurDataURL:
+      "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FBehind.482b2812.jpg&w=5&q=70",
   },
   {
     img: Innovations,
     title: "Innovations in the Game",
     desc: "Inspired by the evolving nature of football, technological advancements transformed the sport. From helmet designs to game strategies, innovation shaped the landscape of American football",
+    blurDataURL:
+      "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FInnovation.0dec7bb5.jpg&w=8&q=70",
   },
   {
     img: Legacy,
     title: "Legacy of Legends",
     desc: "As the season concluded, the legacy of the players lived on. Their stories became tales shared across generations, embodying the essence of American football's cultural significance",
+    blurDataURL:
+      "/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FLegacy.737a4e8b.jpg&w=5&q=70",
   },
 ];
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,57 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image, { StaticImageData } from "next/image";
+import { BiCaretLeftSquare, BiCaretRightSquare } from "react-icons/bi";
+import "./styles.scss";
+import { story } from "./assets";
+
+type storyType = {
+  img: StaticImageData,
+  title: string,
+  desc: string,
+  blurDataURL: string,
+};
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="carousel">
+      <div className="story-cover">
+        {story.map((content: storyType, index: number) => {
+          return (
+            <div className="story" key={index}>
+              <Image
+                src={content.img}
+                alt="story image"
+                width={300}
+                height={300}
+                placeholder="blur"
+                blurDataURL={content.blurDataURL}
+              />
+              <h1>{content.title}</h1>
+              <p>{content.desc}</p>
+            </div>
+          );
+        })}
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="btn-cover">
+        <BiCaretLeftSquare className="left" />
+        <BiCaretRightSquare className="right" />
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="thumbnail-cover">
+        {story.map((content: storyType, index: number) => {
+          return (
+            <div className="thumbnail" key={index}>
+              <Image
+                src={content.img}
+                alt="story image"
+                width={140}
+                height={150}
+                placeholder="blur"
+                blurDataURL={content.blurDataURL}
+              />
+              <h1>{content.title}</h1>
+            </div>
+          );
+        })}
       </div>
-    </main>
+    </div>
   );
 }
